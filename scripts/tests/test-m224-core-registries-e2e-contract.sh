cbccd4b430b04404feee800f730d51460facf292
Jiayou <youjia@Jiayous-MacBook-Pro.local>
2026-09-02 03:22:00 -0700

    test(m2.2.4): require isolated cargo path forwarding

--- scripts/tests/test-m224-core-registries-e2e-contract.sh
+++ scripts/tests/test-m224-core-registries-e2e-contract.sh
@@ -111,6 +111,7 @@ validate_runner_source() {
   assert_contains "$runner_path" 'REAL_RUSTUP_HOME="${RUSTUP_HOME:-$REAL_HOME/.rustup}"' || return 1
   assert_contains "$runner_path" 'CARGO_HOME="$REAL_CARGO_HOME"' || return 1
   assert_contains "$runner_path" 'RUSTUP_HOME="$REAL_RUSTUP_HOME"' || return 1
+  assert_contains "$runner_path" 'PATH="$REAL_CARGO_HOME/bin:$PATH"' || return 1
   assert_line_order "$runner_path" 'rm -rf "$RUN_ROOT"' 'write_meta "$cleanup_ok"' || return 1
   assert_contains "$runner_path" "cmp_snapshots" || return 1
   assert_not_contains "$runner_path" "rm -rf /" || return 1
